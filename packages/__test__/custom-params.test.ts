2e9e93eb7f0b0ecd8cb128211c2458767797dd84
Mr.Bai <935196116@qq.com>
2022-07-18 14:34:43 +0800

    fix: custom params add transform (#306)
    * fix: custom params add transform
    
    * fix: type error
    
    * fix: test
    
    Co-authored-by: Jiwen Bai <56228105@qq.com>

--- packages/__test__/custom-params.test.ts
+++ packages/__test__/custom-params.test.ts
@@ -1,31 +1,109 @@
 import { flushPromises } from '@vue/test-utils'
+import { NInput, NInputNumber } from 'naive-ui'
 import { expect, test } from 'vitest'
+import { h } from 'vue'
+import { useCustomParamsStore } from '../src'
 import { createTest } from './utils'
-import { useCustomParamsStore } from '../src/hooks/use-params'
 
-const customParamsStore = {
-  customParamsStore: useCustomParamsStore({
-    name: null
-  })
+const tableProps = {
+  pagination: {
+    defaultPageSize: 15,
+    defaultPage: 1,
+    itemCount: 0,
+    showSizePicker: true,
+    pageSizes: [15, 20, 50]
+  }
 }
-
-test('test default params', async () => {
-  const { wrapper, router, result } = await createTest(customParamsStore)
+async function createCustomParamsTest(
+  initUrl,
+  initValue: any = { search: null, age: null }
+) {
+  const customParamsStore = useCustomParamsStore(initValue, {
+    age: {
+      type: 'number',
+      transform(v) {
+        return v !== null ? parseInt(v) : null
+      }
+    }
+  })
+  const renderAgeAndSearch = () => {
+    return [
+      h(NInput, {
+        value: customParamsStore.customParamsValue.value.search,
+        onUpdateValue: (v) => customParamsStore.updateCustomParams('search', v)
+      }),
+      h(NInputNumber, {
+        value: customParamsStore.customParamsValue.value.age,
+        onUpdateValue: (v) => customParamsStore.updateCustomParams('age', v)
+      })
+    ]
+  }
+  const { wrapper, result, router } = await createTest(
+    {
+      ...tableProps,
+      customParamsStore: customParamsStore
+    },
+    renderAgeAndSearch,
+    initUrl
+  )
+  return {
+    wrapper,
+    result,
+    router
+  }
+}
+test('test custom params with initUrl', async () => {
+  const { wrapper, result } = await createCustomParamsTest(
+    '/?page.page=1&pageSize.pageSize=14&search.params=asd&age.params=124'
+  )
   await flushPromises()
-  const route = router.currentRoute.value
-
-  expect(wrapper.html()).toContain('Jonny0')
-  expect(result.params).toEqual({ name: null })
-  expect(route.params).toEqual({})
+  const inputNumber = wrapper.findComponent(NInputNumber)
+  const input = wrapper.findComponent(NInput)
+  expect(inputNumber.vm.value).equal(124)
+  expect(input.vm.value).equal('asd')
+  expect(result.params).toEqual({
+    search: 'asd',
+    age: 124
+  })
 })
 
-test('test params change', async () => {
-  customParamsStore.customParamsStore.updateCustomParams('name', 'Tony')
-  const { wrapper, router, result } = await createTest(customParamsStore)
+test('test custom params with initValue', async () => {
+  const { wrapper, result } = await createCustomParamsTest('/', {
+    age: 10,
+    search: 'hello'
+  })
   await flushPromises()
-  const route = router.currentRoute.value
+  const inputNumber = wrapper.findComponent(NInputNumber)
+  const input = wrapper.findComponent(NInput)
+  expect(inputNumber.vm.value).equal(10)
+  expect(input.vm.value).equal('hello')
+  expect(result.params).toEqual({
+    search: 'hello',
+    age: 10
+  })
+})
 
-  expect(wrapper.html()).toContain('CustomTony')
-  expect(result.params).toEqual({ name: 'Tony' })
-  expect(route.query['name.params']).toEqual('Tony')
+test('test custom params with operate', async () => {
+  const { wrapper, result, router } = await createCustomParamsTest('/', {
+    age: 10,
+    search: 'hello'
+  })
+  await flushPromises()
+  const inputNumber = wrapper.findComponent(NInputNumber)
+  const input = wrapper.findComponent(NInput)
+  if (typeof input.vm.onUpdateValue === 'function') {
+    input.vm.onUpdateValue('world' as any)
+  }
+  if (typeof inputNumber.vm.onUpdateValue === 'function') {
+    inputNumber.vm.onUpdateValue(20 as any)
+  }
+  await flushPromises()
+  expect(inputNumber.vm.value).equal(20)
+  expect(input.vm.value).equal('world')
+  expect(result.params).toEqual({
+    search: 'world',
+    age: 20
+  })
+  expect(router.currentRoute.value.query['search.params']).equal('world')
+  expect(router.currentRoute.value.query['age.params']).equal('20')
 })
