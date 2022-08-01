27f0260d1749c6c886517c25ed4afd2e2b88c1f8
Jiwen Bai <56228105@qq.com>
2022-08-01 17:16:46 +0800

    fix: home

--- docs/src/views/home/Home.vue
+++ docs/src/views/home/Home.vue
@@ -1,9 +1,6 @@
 <template>
-  <div style="height: 100%">
-    <n-button
-      style="position: absolute; top: 50%; left: 50%"
-      type="primary"
-      @click="$router.push('/example')"
+  <div style="height: fit-content; text-align: center">
+    <n-button type="primary" @click="$router.push('/example')"
       >开始使用</n-button
     >
   </div>
