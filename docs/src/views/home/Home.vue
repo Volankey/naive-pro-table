e7e11df0f9d515e53209b5c52f216b37dd8b9155
Jiwen Bai <56228105@qq.com>
2022-09-19 14:18:59 +0800

    fix: build docs error

--- docs/src/views/home/Home.vue
+++ docs/src/views/home/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div style="height: fit-content; text-align: center">
-    <n-button type="primary" @click="$router.push('/example')"
-      >开始使用</n-button
-    >
+    <n-button type="primary" @click="handleClick">开始使用</n-button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function handleClick() {
+  router.push('/example')
+}
+</script>
