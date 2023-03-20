6d2911dc84c2487a45dbd15a3d291f7250e0feb1
congjin.li <congjin.li@tusen.ai>
2023-03-20 11:42:47 +0800

    feat: 新增路由查询hook

---
+++ packages/src/hooks/use-custom-router-query/preset.ts
@@ -0,0 +1,90 @@
+import dayjs from 'dayjs'
+
+export const numberPreset = {
+  render(value: number | undefined | null) {
+    return value?.toString()
+  },
+  getFromQuery(routerQuery: string | undefined) {
+    if (routerQuery?.length) {
+      return parseInt(routerQuery)
+    }
+    return undefined
+  }
+}
+
+export const datePreset = {
+  render(value: number) {
+    if (value) {
+      const temp = dayjs(value).format('YYYY-MM-DD')
+      return temp
+    }
+  },
+  getFromQuery(routerQuery: string | undefined) {
+    if (
+      routerQuery?.length &&
+      dayjs(routerQuery, 'YYYY-MM-DD', true).isValid()
+    ) {
+      return dayjs(routerQuery, 'YYYY-MM-DD').valueOf()
+    }
+    return undefined
+  }
+}
+
+export const dateRangePreset = {
+  render(value: number[] | null) {
+    if (value) {
+      const tempDate = value?.map((item: number) =>
+        dayjs(item).format('YYYY-MM-DD')
+      )
+      return JSON.stringify(tempDate)
+    }
+  },
+  getFromQuery(routerQuery: string | undefined) {
+    let res
+    try {
+      res =
+        (routerQuery?.length &&
+          JSON.parse(routerQuery)?.map((item: string) => {
+            if (dayjs(item, 'YYYY-MM-DD', true).isValid()) {
+              return dayjs(item, 'YYYY-MM-DD').valueOf()
+            }
+            console.error(item + '路由时间解析失败，返回undefined')
+            throw new Error(item + '路由时间解析失败，返回undefined')
+          })) ??
+        undefined
+    } catch {
+      console.warn('路由日期不合法，返回undefined')
+      return undefined
+    }
+    return res
+  }
+}
+
+export const booleanPreset = {
+  render(value: boolean | undefined) {
+    return value ? 'true' : 'false'
+  },
+  getFromQuery(routerQuery: string | undefined) {
+    if (routerQuery?.length) {
+      return routerQuery !== 'false' //路由解析当false或者undefined的时候为false，其他都为true
+    }
+    return false
+  }
+}
+
+export const stringArrayPreset = {
+  render(value: string[] | undefined) {
+    if (value) {
+      return JSON.stringify(value)
+    }
+  },
+  getFromQuery(routerQuery: string | undefined) {
+    if (routerQuery?.length) {
+      const parse = JSON.parse(routerQuery)
+      if (!(parse instanceof Array)) {
+        throw new Error('非数组')
+      }
+      return parse
+    }
+  }
+}
