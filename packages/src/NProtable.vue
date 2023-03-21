14f2f258a370aaa5cefc78b58a8bb74dc9793e25
congjin.li <congjin.li@tusen.ai>
2023-03-21 14:17:44 +0800

    fix: 引入方式

--- packages/src/NProtable.vue
+++ packages/src/NProtable.vue
@@ -19,7 +19,7 @@ import { getColumnsRouteRules, handleColumn, useTableRequest } from './utils'
 import { TableParamsStore } from './table-params-store'
 import { syncFromRouter, syncRouterQuery } from './router-sync'
 import type { QueryOptions } from './table-params-store/types'
-import { debounce } from 'lodash-es'
+import debounce from 'lodash-es/debounce'
 import { CustomParams } from './hooks'
 import type { DateFormatter } from './value-type-render/interface'
 
